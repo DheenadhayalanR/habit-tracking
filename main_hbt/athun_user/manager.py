9692b56d96a74fde533e441fdf98bf07df9c09e9
DheenadhayalanR <116442260+DheenadhayalanR@users.noreply.github.com>
2025-06-01 13:04:16 +0530

    Update manager.py

--- main_hbt/athun_user/manager.py
+++ main_hbt/athun_user/manager.py
@@ -3,9 +3,9 @@ from django.contrib.auth.hashers import make_password
 
 class Usermanager(BaseUserManager):
     
-    def create_user(self,username,email,phone,password, **extra_fields):
+    def create_user(self,username,email,password, **extra_fields):
         email=self.normalize_email(email)
-        user = self.model(username=username, email=email,phone=phone, **extra_fields)
+        user = self.model(username=username, email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user    
