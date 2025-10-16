66e823b31c18abebcc906018bf118110eccdad5d
DheenadhayalanR <dheenadhayal444@gmail.com>
2025-10-17 00:01:58 +0530

    This code is working in local system

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 running command - npx react-native run-android
+
+
+// Remove the migration file -->  for /d /r . %i in (migrations) do if exist "%i" rd /s /q "%i"
+
+
+// Load data from fixtures folder --> python manage.py loaddata ./fixtures/name.json
+ 
+ (eg..)->Data ([
+                {
+                    "model": "quest_app.DayPlan",
+                    "pk": 1,
+                    "fields": {
+                        "day_name": "Day 1"
+                    }
+                },
+                {
+                    "model": "quest_app.DayPlan",
+                    "pk": 2,
+                    "fields": {
+                        "day_name": "Day 2"
+                    }
+                }])
+
+
+// Command --> python manage.py file_name 
+
+eg .. -> management -> commands -> file_name.py
+
+
--- main_hbt/main_hbt/settings.py
+++ main_hbt/main_hbt/settings.py
@@ -23,16 +23,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY ='django-insecure-qp*l2ado7n*s9^qs*!l=s1v_v%ptvl$8**=+wo_@!lq^n2xr_p'
+SECRET_KEY ='django-insecure-qp*l2ado7n*s9^qs*!l=s1v_v%ptvl$8**=+wo_@!lq^n2xr_p'
 
-SECRET_KEY = os.environ.get("SECRET_KEY")                        
+# SECRET_KEY = os.environ.get("SECRET_KEY")                        
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = os.environ.get("DEBUG","False").lower() == "true"
+DEBUG = True
+# DEBUG = os.environ.get("DEBUG","False").lower() == "true"
 
-# ALLOWED_HOSTS =['*']
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split()
+ALLOWED_HOSTS =['*']
+# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split()
 
 # Application definition
 
@@ -100,18 +100,18 @@ WSGI_APPLICATION = 'main_hbt.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'hbt',
-#         "USER": "root",
-#         "PASSWORD": "toor121.",
-#         'HOST':'localhost',
-#         'PORT':'3306',
-#     }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hbt',
+        "USER": "root",
+        "PASSWORD": "toor121.",
+        'HOST':'localhost',
+        'PORT':'3306',
+    }
 }
 # database_url='postgresql://toor:KpMoncc2Tor5veliDM3XLadAPLIa1BsV@dpg-d11990p5pdvs73eo5ulg-a.oregon-postgres.render.com/hbtrack_m2bc'
-database_url = os.environ.get("DATABASES_URL")
-DATABASES['default'] = dj_database_url.parse(database_url)
+# database_url = os.environ.get("DATABASES_URL")
+# DATABASES['default'] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
