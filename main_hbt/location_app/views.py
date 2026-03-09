b8117f50a6fc25318f00ac2463f625e95b17aef2
DheenadhayalanR <dheenadhayal444@gmail.com>
2026-03-09 23:47:39 +0530

    Refactor the location view functionn

--- main_hbt/location_app/views.py
+++ main_hbt/location_app/views.py
@@ -37,11 +37,9 @@ class LocationView(generics.ListAPIView):
     def list(self, request, *args, **kwargs):
 
         tracked_ip = get_client_ip(request)
-        geolocation_data = get_ip_geolocation(tracked_ip)
-        logger.info("geolocation_data",geolocation_data)
-        logger.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+        geolocation_data = get_ip_geolocation(tracked_ip) 
+       
         if geolocation_data and "error" not in geolocation_data:
-
             loc = geolocation_data.get("loc")
 
             if loc:
@@ -53,14 +51,16 @@ class LocationView(generics.ListAPIView):
 
                 location_name = f"{country}, {region}, {city}"
 
-                # Location.objects.get_or_create(
-                #     user=request.user,
-                #     defaults={
-                #         "latitude": latitude,
-                #         "longitude": longitude,
-                #         "country_region_city_name": location_name
-                #     }
-                # )
+                print("location_name",request.user.id)    
+
+                Location.objects.get_or_create(
+                    user=request.user.id,
+                    defaults={
+                        "latitude": latitude,
+                        "longitude": longitude,
+                        "country_region_city_name": location_name
+                    }
+                )
 
         return super().list(request, *args, **kwargs)
 
